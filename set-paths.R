692c7d16de8832d4ecb2575928d5b0a82109f247
lhe2 <97200903+lhe2@users.noreply.github.com>
2025-02-19 21:43:34 -0500

    rename `set-paths.R`, add 2023 paths

---
+++ set-paths.R
@@ -0,0 +1,19 @@
+# 2025-02-19
+# title: "set-2024-paths.R"
+
+# pathfinding with 'here'
+
+here::i_am("set-2024-paths.R")
+library(here)
+
+bin_paths24 <- list(y24 = here("2024"),
+                    data = here("2024", "data"),
+                    bin = here("2024", "00-bin"),
+                    util = here("2024", "00-util"),
+                    clean = here("2024", "01-cleaning"),
+                    wrangle = here("2024", "02-wrangle"),
+                    do = here("2024", "03-analysis"))
+
+bin_paths23 <- list(y23 = here("2023"),
+                    data = here("2023", "data"))
+
