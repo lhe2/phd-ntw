7a078ff3579278d0560b49c91f7e01c0fdc09ae1
lhe2 <97200903+lhe2@users.noreply.github.com>
2025-08-14 20:29:10 -0400

    tdt file path updating

--- set-paths.R
+++ set-paths.R
@@ -2,14 +2,14 @@
 # title: "set-2024-paths.R"
 
 # pathfinding with 'here'
+# no "-" in the list name to prevent breaking!!!
 
 here::i_am("set-paths.R")
 library(here)
 
 bin_paths <- list(y25 = list(root = here("2025"),
                              tdt = here("2025", "tdt"),
-                             tdt-data = here("2025", "tdt", "data"),
-                             ),
+                             tdtdata = here("2025", "tdt", "data")),
                   
                   y24 = list(root = here("2024"),
                              data = here("2024", "data"),
@@ -20,18 +20,4 @@ bin_paths <- list(y25 = list(root = here("2025"),
                   
                   y23 = list(root = here("2023"),
                              data = here("2023", "data")))
-
-
-# bin_paths25 <- list(y25 = here("2025"),
-#                     data = here("2025", "data"))
-# 
-# bin_paths24 <- list(y24 = here("2024"),
-#                     data = here("2024", "data"),
-#                     clean = here("2024", "01-cleaning"),
-#                     wrangle = here("2024", "02-wrangle"),
-#                     doviz = here("2024", "03-viz"),
-#                     dostats = here("2024", "03-stats"))
-# 
-# bin_paths23 <- list(y23 = here("2023"),
-#                     data = here("2023", "data"))
 
