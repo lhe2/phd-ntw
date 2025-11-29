e70aacfb6d25258c73bc32cfd726dc6d5c6c10d8
lhe2 <97200903+lhe2@users.noreply.github.com>
2025-11-29 15:43:57 -0500

    update ntw 2025 util fns documentation + functioning; list-ify ntw 2025 wrangling

--- 2025/ntw/utils.R
+++ 2025/ntw/utils.R
@@ -4,29 +4,32 @@
 
 # readme ------------------------------------------------------------------
 
-# utility functions for ntw wrangling/analyses/etc
+# utility and user-defined functions for ntw wrangling/analyses/etc 
+# cuz i always lose them in the sauce
 
 
 
 # wrangle -----------------------------------------------------------------
 
-.StandardiseColTypes <- function(data){
+## cleaning -----------------------------------------------------------------
+StdiseColTypes <- function(data){
   data %>% 
     mutate(trt = as.numeric(trt),
-         id = as.numeric(id),
-         across(starts_with("jdate"), as.numeric))
+           id = as.numeric(id),
+           across(starts_with("jdate"), as.numeric))
 }
 
-
-.RecodeDevOutcomes <- function(data){
+# for sup, only cares abt things reaching at least 5th
+CodeDevOutcomes <- function(data){
   data %>%
     mutate(fate.code = case_when(fate.dev %in% c("ec", "pup") ~ 1,
                                  fate.dev == "pmd" ~ 0,
                                  fate.dev == "other" ~ 2),
-           sup = case_when(!is.na(jdate.pmd) & !is.na(jdate.7th) ~ 7,
-                           !is.na(jdate.pmd) & !is.na(jdate.6th) ~ 6,
-                           !is.na(jdate.pmd) & !is.na(jdate.5th) ~ 0,
-                           FALSE ~ sup
+           sup = case_when(!is.na(jdate.8th) ~ 8,
+                           !is.na(jdate.7th) ~ 7,
+                           !is.na(jdate.6th) ~ 6,
+                           !is.na(jdate.5th) ~ 5,
+                           #FALSE ~ as.numeric(sup)
            )
     )
 }
