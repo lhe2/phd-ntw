ddf6f2fb4b2f8fc2f2945814a665c6cc4cf0e2bf
lhe2 <97200903+lhe2@users.noreply.github.com>
2025-11-29 19:54:41 -0500

    rename ntw25 "wrangle" to "cleaning"; update ntw25 utils

--- 2025/ntw/utils.R
+++ 2025/ntw/utils.R
@@ -9,27 +9,69 @@
 
 
 
-# wrangle -----------------------------------------------------------------
 
-## cleaning -----------------------------------------------------------------
-StdiseColTypes <- function(data){
-  data %>% 
-    mutate(trt = as.numeric(trt),
-           id = as.numeric(id),
-           across(starts_with("jdate"), as.numeric))
-}
-
-# for sup, only cares abt things reaching at least 5th
-CodeDevOutcomes <- function(data){
+RmLabTB <- function(data){
   data %>%
-    mutate(fate.code = case_when(fate.dev %in% c("ec", "pup") ~ 1,
-                                 fate.dev == "pmd" ~ 0,
-                                 fate.dev == "other" ~ 2),
-           sup = case_when(!is.na(jdate.8th) ~ 8,
-                           !is.na(jdate.7th) ~ 7,
-                           !is.na(jdate.6th) ~ 6,
-                           !is.na(jdate.5th) ~ 5,
-                           #FALSE ~ as.numeric(sup)
-           )
-    )
+    filter(pop == "lab" & diet == "TB")
 }
+
+
+
+
+##### dont need a lot of this stuff ooops cuz i changed things
+# cleaning -----------------------------------------------------------------
+
+# moved out into the script itself 
+
+# StdiseColTypes <- function(data){
+#   data %>% 
+#     mutate(trt = as.numeric(trt),
+#            id = as.numeric(id),
+#            across(starts_with("jdate"), as.numeric))
+# }
+# 
+# # for sup, only cares abt things reaching at least 5th
+# CodeDevOutcomes <- function(data){
+#   data %>%
+#     mutate(fate.code = case_when(fate.dev %in% c("ec", "pup") ~ 1,
+#                                  fate.dev == "pmd" ~ 0,
+#                                  fate.dev == "other" ~ 2),
+#            sup = case_when(!is.na(jdate.8th) ~ 8,
+#                            !is.na(jdate.7th) ~ 7,
+#                            !is.na(jdate.6th) ~ 6,
+#                            !is.na(jdate.5th) ~ 5,
+#                            #FALSE ~ as.numeric(sup)
+#            )
+#     )
+# }
+
+# wrangle -----------------------------------------------------------------
+# dont need anymore bc combined everything lol
+
+
+# TODO include exit date calcs if culled, survived thru adult, pmd, etc? 
+
+# .CalcTT <- function(long_data){
+#   long_data %>%
+#     mutate(tt.3rd = jdate.3rd - jdate.hatch,
+#            tt.4th = jdate.4th - jdate.hatch,
+#            tt.5th = jdate.5th - jdate.hatch,
+#            tt.6th = jdate.6th - jdate.hatch,
+#            tt.7th = jdate.7th - jdate.hatch,
+#            tt.8th = jdate.8th - jdate.hatch,
+#            tt.wander = jdate.wander - jdate.hatch,
+#            tt.pupa = jdate.pupa - jdate.hatch,
+#            tt.eclose = jdate.eclose - jdate.hatch,
+#     )
+# }
+
+# .PivotLongerDev <- function(wide_data){
+#   wide_data %>%
+#     pivot_longer(cols = starts_with(c("jdate", "mass", "tt")),
+#                  names_to = c(".value", "instar"),
+#                  values_drop_na = TRUE,
+#                  names_pattern = ("([a-z]*\\d*)\\.(\\d*[a-z]*)")) %>%
+#     drop_na(jdate) %>%
+#     drop_na(tt) # drops NAs if individual didn't reach a certain stage
+# }
+
