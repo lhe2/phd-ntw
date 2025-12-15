04e98677f895089e18814a223177c2beef87d846
lhe2 <97200903+lhe2@users.noreply.github.com>
2025-12-15 16:21:23 -0500

    minor changes to QOL and documentation of ntw25 utils/scripts
    - add df factorising to start of viz
    - include `%H%M` to purl archiving
    - reorg utils script

--- 2025/ntw/utils.R
+++ 2025/ntw/utils.R
@@ -7,17 +7,32 @@
 # utility and user-defined functions for ntw wrangling/analyses/etc 
 # cuz i always lose them in the sauce
 
-# functions -----------------------------------------------------------------
-### MATH ###
+
+# WRANGLE -----------------------------------------------------------------
 se <- function(x){ 
   sd(na.omit(x))/sqrt(length(na.omit(x)))
 }
 
+# where p = x/n
 se.prop <- function(p, n){
   sqrt((p*(1-p))/n)
 }
 
-### PLOTTING ###
+
+# DATA VIZ ----------------------------------------------------------------
+### FILTERS ###
+FilterOutLabTB <- function(data){
+  data %>%
+    filter(!(pop == "lab" & diet == "TB"))
+}
+
+FilterForLabEggs <- function(data){
+  data %>%
+    filter(mate.pop != "field",
+           mate.type != "virgin f") 
+}
+
+### GGPLOT ###
 # sets error bar width/height to some % of the other axis
 ## struggling w ggplot error bar width issues again... 
 ## see also https://gist.github.com/tomhopper/9076152 for suggested defaults
@@ -30,23 +45,6 @@ CalcErrHt <- function(y, pct = 0.07){
   max(y) * pct
 }
 
-### CONVENIENCE ###
-FilterOutLabTB <- function(data){
-  data %>%
-    filter(!(pop == "lab" & diet == "TB"))
-}
-
-FilterForLabEggs <- function(data){
-  data %>%
-    filter(mate.pop != "field",
-           mate.type != "virgin f") 
-}
-
-
-# GroupNTW <- function(data){
-#   data %>%
-#     group_by(year, pop, diet, trt)
-# }
 
 
 
