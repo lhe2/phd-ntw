e59c5b8a135f55c7c6f892a09fc5285d17a91e09
lhe2 <97200903+lhe2@users.noreply.github.com>
2025-12-17 18:26:06 -0500

    update general ntw25 utils

--- 2025/ntw/utils/general.R
+++ 2025/ntw/utils/general.R
@@ -2,24 +2,14 @@
 # 2025-11-28
 
 
-# readme ------------------------------------------------------------------
+### ABOUT ###
 
-# utility and user-defined functions for ntw wrangling/analyses/etc 
-# cuz i always lose them in the sauce
+# general utility functions that are useful at across multiple scripts
+# (mostly post-wrangle)
 
-
-# WRANGLE -----------------------------------------------------------------
-se <- function(x){ 
-  sd(na.omit(x))/sqrt(length(na.omit(x)))
-}
-
-# where p = x/n
-se.prop <- function(p, n){
-  sqrt((p*(1-p))/n)
-}
+############
 
 
-# ANALYSIS ----------------------------------------------------------------
 ### FILTERS ###
 FilterOutLabTB <- function(data){
   data %>%
@@ -33,28 +23,6 @@ FilterForLabEggs <- function(data){
 }
 
 
-### STATS ###
-GetModelResults <- function(x){
-  list(
-    model_summary = summary(x),
-    #anova_results = anova(x),
-    anova_wchisq = anova(x, test = "Chisq")
-  )
-}
-
-
-### VIZ (GGPLOT) ###
-# sets error bar width/height to some % of the other axis
-## struggling w ggplot error bar width issues again... 
-## see also https://gist.github.com/tomhopper/9076152 for suggested defaults
-
-CalcErrWd <- function(x, pct = 0.12){
-  max(x) * pct 
-}
-
-CalcErrHt <- function(y, pct = 0.07){
-  max(y) * pct
-}
 
 
 
