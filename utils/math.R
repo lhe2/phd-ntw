02f450b6859860699e58ddd40d9a8086a6e3fa98
lhe2 <97200903+lhe2@users.noreply.github.com>
2026-01-05 19:18:10 -0500

    refactor: adding final minor notes + changes to other ntw scripts before updating main branch
    - notes on import (23 tents, 24 dev, 25 tents); wrangle util; tents viz

--- utils/math.R
+++ utils/math.R
@@ -1,13 +1,14 @@
-# ntw 2025 wrangle utils
-# 2025-11-28
+# math utils.R
+# created: 2026-01-01
 
-# (probably ok to supercede w the math utils)
+# frequently used calculation fns
 
-### SUMMARY STATS ###
+# std error
 se <- function(x){ 
   sd(na.omit(x))/sqrt(length(na.omit(x)))
 }
 
+# std error of a proportion
 # where p = x/n
 se.prop <- function(p, n){
   sqrt((p*(1-p))/n)
