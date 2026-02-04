d3e27620e463525146039e1de21941044eff17e8
lhe2 <97200903+lhe2@users.noreply.github.com>
2026-02-04 16:02:03 -0500

    refactor/feat: update ntw stats analysis utils
    - update `GetModelResults()` to work on single (i.e. non-list) objects
    - add `DiagnoseModel()` to generate plots for looking at resids

--- scripts/utils/analysis-stats.R
+++ scripts/utils/analysis-stats.R
@@ -1,12 +1,30 @@
 # ntw 2025 stats utils
 # 2025-11-28
 
+# checks if the object being passed in is a list or not
+# (i.e. list of fitted model objs or single obj -- converts to list if not)
 
 # simultaneously return model summary and anova results
 GetModelResults <- function(x){
-  list(
-    model_summary = summary(x),
-    #anova_results = anova(x),
-    anova_wchisq = anova(x, test = "Chisq")
-  )
+  if(inherits(x, "list") == FALSE){
+    x <- list(x)
+  }
+  
+  x %>% lapply(., \(x){
+    list(model_summary = summary(x),
+         anova_wchisq = anova(x, test = "Chisq"))
+    })
 }
+
+# generate fitted vs resid; QQ plot
+## (need to load ggfortify if not working)
+DiagnoseModel <- function(x){
+  if(inherits(x, "list") == FALSE){
+    x <- list(x)
+  } 
+  
+  x %>% lapply(., \(x){
+    list(autoplot(x, which = 1:2, na.action="fail"))
+  })
+}
+
