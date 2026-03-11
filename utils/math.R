00a464d92758ce014edd9615b64f91aad959bad8
lhe2 <97200903+lhe2@users.noreply.github.com>
2026-03-11 15:19:10 -0400

    refactor: separate out math utils to project root
    - (kept bothering me)
    - also rename `se.prop()` to `seprop()`

--- utils/math.R
+++ utils/math.R
@@ -1,13 +1,13 @@
-# ntw 2025 wrangle utils
-# 2025-11-28
-
-# (probably ok to supercede w the math utils)
+# global wrangle util fns
+# 2026-03-11
 
 ### SUMMARY STATS ###
+# standard error
 se <- function(x){ 
   sd(na.omit(x))/sqrt(length(na.omit(x)))
 }
 
+# standard error of a proportion
 # where p = x/n
 seprop <- function(p, n){
   sqrt((p*(1-p))/n)
